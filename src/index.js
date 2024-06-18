23a4468a7f5498c08755d70100e585b44660af4d
tuanptit2002 <tuantienti952002@gmail.com>
2024-06-18 17:32:04 +0700

    fix-login
    Signed-off-by: tuanptit2002 <tuantienti952002@gmail.com>

--- src/index.js
+++ src/index.js
@@ -21,8 +21,13 @@ app.use(function(req, res, next) {
     resave: false,
     saveUninitialized: true
   }));
-  app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
-  app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+const corsOptions = {
+    origin: ['http://localhost:8080', 'http://localhost:5173'],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
+
   app.use(passport.initialize());
   app.use(passport.session());
 app.use(express.json())
--- src/models/user.js
+++ src/models/user.js
@@ -74,6 +74,17 @@ userSchema.methods.generateAuthToken = async function () {
     return token;
 }
 
+userSchema.statics.findBycredentials  = async (email, password) => {
+    const user  = await User.findOne({email})
+    if(!user){
+        throw new Error('Unable to login')
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        throw new Error('Unable to login')
+    }
+    return user;
+}
 
 userSchema.pre('save', async function(next) {
     const user = this;
